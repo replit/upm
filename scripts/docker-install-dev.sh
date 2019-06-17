6e034ee40ec0f4966e52b781ea9f26284faeaf9f
Radon Rosborough <radon.neon@gmail.com>
2019-06-17 16:14:14 -0700

    Reformat APK packages list

--- scripts/docker-install-dev.sh
+++ scripts/docker-install-dev.sh
@@ -3,7 +3,20 @@
 set -e
 set -o pipefail
 
-apk add --no-cache curl emacs git go make musl-dev python2 python3
+packages="
+
+curl
+emacs
+git
+go
+make
+musl-dev
+python2
+python3
+
+"
+
+apk add --no-cache $packages
 pip3 --disable-pip-version-check install poetry
 curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python3
 ln -s "$HOME/.cask/bin/cask" /usr/local/bin/
