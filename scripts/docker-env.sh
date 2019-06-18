9a01caa9874137e621883e838baaa1c1ba4f7fb8
Radon Rosborough <radon.neon@gmail.com>
2019-06-18 10:33:38 -0700

    Add more aliases to 'make docker' shell

--- scripts/docker-env.sh
+++ scripts/docker-env.sh
@@ -2,4 +2,15 @@
 
 export PATH="/upm/cmd/upm:$PATH"
 cd /upm
-alias u='(cd /upm && make -s upm) && upm'
+
+# Alias to force rebuilding UPM.
+alias b='make -s -C /upm clean upm'
+
+# Alias to build and run the latest version of UPM.
+alias u='make -s -C /upm upm && upm'
+
+# Alias to generate and cd to a temporary directory, for testing UPM.
+alias mt='cd "$(mktemp -d)"'
+
+# Alias for listing directories.
+alias l='ls -lA'
