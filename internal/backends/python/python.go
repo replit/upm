9ef5bafd04f63c6d1608389f08c1d18dc660e54b
sergeichestakov <schestakov@ucdavis.edu>
2020-11-12 16:47:33 -0800

    Use PyPI REST API for fetching Python package info

--- internal/backends/python/python.go
+++ internal/backends/python/python.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
 
 	"github.com/BurntSushi/toml"
@@ -17,26 +16,29 @@ import (
 // moduleToPypiPackage pypiPackageToModules are provided
 //go:generate go run ./gen_pypi_map -from pypi_packages.json -pkg python -out pypi_map.gen.go
 
-// pypiXMLRPCEntry represents one element of the response we get from
-// the PyPI XMLRPC API on doing a search.
-type pypiXMLRPCEntry struct {
+// pypiEntry represents one element of the response we get from
+// the PyPI API search results.
+type pypiEntry struct {
 	Name    string `json:"name"`
 	Summary string `json:"summary"`
 	Version string `json:"version"`
 }
 
-// pypiXMLRPCInfo represents the response we get from the PyPI XMLRPC
-// API on doing a single-package lookup.
-type pypiXMLRPCInfo struct {
-	Author       string   `json:"author"`
-	AuthorEmail  string   `json:"author_email"`
-	HomePage     string   `json:"home_page"`
-	License      string   `json:"license"`
-	Name         string   `json:"name"`
-	ProjectURL   []string `json:"project_url"`
-	RequiresDist []string `json:"requires_dist"`
-	Summary      string   `json:"summary"`
-	Version      string   `json:"version"`
+// pypiEntryInfo represents the response we get from the
+// PyPI API on doing a single-package lookup.
+type pypiEntryInfo struct {
+	Author        string   `json:"author"`
+	AuthorEmail   string   `json:"author_email"`
+	HomePage      string   `json:"home_page"`
+	License       string   `json:"license"`
+	Name          string   `json:"name"`
+	ProjectURL    string   `json:"project_url"`
+	PackageURL    string   `json:"package_url"`
+	BugTrackerURL string   `json:"bugtrack_url"`
+	DocsURL       string   `json:"docs_url"`
+	RequiresDist  []string `json:"requires_dist"`
+	Summary       string   `json:"summary"`
+	Version       string   `json:"version"`
 }
 
 // pyprojectTOML represents the relevant parts of a pyproject.toml
@@ -170,7 +172,7 @@ func pythonMakeBackend(name string, python string) api.LanguageBackend {
 				util.GetResource("/python/pypi-search.py"),
 				query,
 			})
-			var outputJSON []pypiXMLRPCEntry
+			var outputJSON []pypiEntry
 			if err := json.Unmarshal(outputB, &outputJSON); err != nil {
 				util.Die("PyPI response: %s", err)
 			}
@@ -190,56 +192,23 @@ func pythonMakeBackend(name string, python string) api.LanguageBackend {
 				util.GetResource("/python/pypi-info.py"),
 				string(name),
 			})
-			var output pypiXMLRPCInfo
+			var output pypiEntryInfo
 			if err := json.Unmarshal(outputB, &output); err != nil {
 				util.Die("PyPI response: %s", err)
 			}
+
 			info := api.PkgInfo{
-				Name:        output.Name,
-				Description: output.Summary,
-				Version:     output.Version,
-				HomepageURL: output.HomePage,
+				Name:             output.Name,
+				Description:      output.Summary,
+				Version:          output.Version,
+				HomepageURL:      output.HomePage,
+				DocumentationURL: output.DocsURL,
+				BugTrackerURL:    output.BugTrackerURL,
 				Author: util.AuthorInfo{
 					Name:  output.Author,
 					Email: output.AuthorEmail,
 				}.String(),
 				License: output.License,
-			}
-			for _, line := range output.ProjectURL {
-				fields := strings.SplitN(line, ", ", 2)
-				if len(fields) != 2 {
-					continue
-				}
-
-				name := fields[0]
-				url := fields[1]
-
-				matched, err := regexp.MatchString(`(?i)doc`, name)
-				if err != nil {
-					panic(err)
-				}
-				if matched {
-					info.DocumentationURL = url
-					continue
-				}
-
-				matched, err = regexp.MatchString(`(?i)code`, name)
-				if err != nil {
-					panic(err)
-				}
-				if matched {
-					info.SourceCodeURL = url
-					continue
-				}
-
-				matched, err = regexp.MatchString(`(?i)track`, name)
-				if err != nil {
-					panic(err)
-				}
-				if matched {
-					info.BugTrackerURL = url
-					continue
-				}
 			}
 
 			deps := []string{}
--- resources/python/pypi-info.py
+++ resources/python/pypi-info.py
@@ -1,24 +1,23 @@
-# This is a Python script that looks up package metadata on PyPI using
-# the XMLRPC API. It takes one argument, the name of the package (not
+# This is a Python script that looks up package metadata on PyPI.
+# It takes one argument, the name of the package (not
 # necessarily canonical), and outputs the results in JSON format (a
-# map, see pypiXMLRPCInfo). The script works on both Python 2 and
-# Python 3.
+# map). The script works on both Python 2 and Python 3.
 
 from __future__ import print_function
+import requests
 import json
 import sys
 
-try:
-    from xmlrpc import client as xmlrpc
-except ImportError:
-    import xmlrpclib as xmlrpc
+if len(sys.argv) != 2:
+    print("Invalid number of arguments. Usage: python pypy-info.py pkg")
+    sys.exit(1)
 
 package = sys.argv[1]
-pypi = xmlrpc.ServerProxy("https://pypi.org/pypi")
-releases = pypi.package_releases(package)
-if not releases:
+
+result = requests.get("https://pypi.org/pypi/%s/json"% package)
+if result.status_code != 200:
     print("{}")
     sys.exit(0)
-release, = releases
-info = pypi.release_data(package, release)
+
+info = result.json()['info']
 json.dump(info, sys.stdout)
