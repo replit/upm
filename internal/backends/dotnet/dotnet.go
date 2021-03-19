9f3e528447368bd86e294621721f66eef9012bd4
navtenie <navteniev@linkedin.com>
2021-03-19 15:01:06 -0400

    Added support for list command

--- internal/backends/dotnet/dotnet.go
+++ internal/backends/dotnet/dotnet.go
@@ -1,6 +1,7 @@
 package dotnet
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"strings"
@@ -8,6 +9,24 @@ import (
 	"github.com/replit/upm/internal/api"
 	"github.com/replit/upm/internal/util"
 )
+
+type packageReference struct {
+	XMLName xml.Name `xml:"PackageReference"`
+	Include string   `xml:"Include,attr"`
+	Version string   `xml:"Version,attr"`
+}
+
+type propertyGroup struct {
+	XMLName         xml.Name `xml:"PropertyGroup"`
+	OutputType      string   `xml:"OutputType"`
+	TargetFramework string   `xml:"TargetFramework"`
+}
+
+type project struct {
+	XMLName       xml.Name           `xml:"Project"`
+	PropertyGroup propertyGroup      `xml:"PropertyGroup"`
+	Packages      []packageReference `xml:"ItemGroup>PackageReference"`
+}
 
 func removePackages(pkgs map[api.PkgName]bool) {}
 
@@ -42,6 +61,26 @@ func listSpecfile() map[api.PkgName]api.PkgSpec {
 	projectFile := findSpecFile()
 	if util.Exists(projectFile) {
 		util.ProgressMsg(fmt.Sprintf("Found %s", projectFile))
+		var project project
+		var xmlbytes []byte
+		var err error
+
+		xmlbytes, err = ioutil.ReadFile(projectFile)
+		util.ProgressMsg("Read inputfile")
+		if err != nil {
+			util.Die("error reading spec file: %s", err)
+		}
+		err = xml.Unmarshal(xmlbytes, &project)
+		util.ProgressMsg("unmarshed xml")
+		if err != nil {
+			util.Die("error unmarshaling spec file: %s", err)
+		}
+		util.ProgressMsg(fmt.Sprintf("Project: %s", project))
+		for _, packageReference := range project.Packages {
+			pkgName := packageReference.Include
+			pkgVersion := api.PkgVersion(packageReference.Version)
+			pkgs[api.PkgName(pkgName)] = api.PkgSpec(pkgVersion)
+		}
 	}
 	return pkgs
 }
