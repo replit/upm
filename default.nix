752d7c3fc348c452185f6ae45106f62ccc2b85ea
Connor Brewster <cbrewster@hey.com>
2021-06-25 19:04:03 -0500

    Fix nix build (#72)
    * Fix nix build
    
    * some cleanup

--- default.nix
+++ default.nix
@@ -1,26 +1,21 @@
 { pkgs ? import <nixpkgs>{}, versionArg ? "" } :
 let
-    inherit(pkgs)
-        buildGoModule
-        statik
-        git
-        runCommand;
-in
-let
+
     src = pkgs.copyPathToStore ./.;
-    revision = runCommand "get-rev" {
-        nativeBuildInputs = [ git python3 ];
+
+    revision = pkgs.runCommand "get-rev" {
+        nativeBuildInputs = with pkgs; [ git ];
         # impure, do every time, see https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/fetchgitlocal/default.nix#L9
         dummy = builtins.currentTime;
     } ''
         if [ -d ${src}/.git ]; then
-            cd ${src}
-            git rev-parse --short HEAD | tr -d '\n' > $out
+            git --git-dir="${src}/.git" -C "${src}" rev-parse --short HEAD | tr -d '\n' > $out
         else
             echo ${versionArg} | tr -d '\n' > $out
         fi
     '';
-in buildGoModule {
+
+in pkgs.buildGoModule {
     pname = "upm";
     version = builtins.readFile revision;
 
@@ -29,7 +24,7 @@ in buildGoModule {
     vendorSha256 = "1fjk4wjcqdkwhwgvx907pxd9ga8lfa36xrkh64ld5b8d0cv62mzv";
 
     preBuild = ''
-        ${statik}/bin/statik -src resources -dest internal -f
+        ${pkgs.statik}/bin/statik -src resources -dest internal -f
         go generate ./internal/backends/python
     '';
 
