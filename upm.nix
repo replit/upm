c1d753249ecc57eeba550aa88f488d40384d7767
Connor Brewster <cbrewster@hey.com>
2021-07-06 08:09:04 -0600

    Inject version into cli

--- upm.nix
+++ upm.nix
@@ -8,6 +8,10 @@ buildGoModule {
 
     vendorSha256 = "1fjk4wjcqdkwhwgvx907pxd9ga8lfa36xrkh64ld5b8d0cv62mzv";
 
+    buildFlagsArray = [
+      "-ldflags=-X github.com/replit/upm/internal/cli.version=${rev}"
+    ];
+
     preBuild = ''
         ${statik}/bin/statik -src resources -dest internal -f
         go generate ./internal/backends/python
