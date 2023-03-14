8526d278fd3c604cedc9749aee3a1cde5361c878
Colton Donnelly <colton@donn.io>
2023-03-14 01:16:08 +0000

    script to generate pkgs_to_test.json

---
+++ internal/backends/python/gen_top_packages_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+def getOutputFile():
+	flag = sys.argv.index('--output')
+	return sys.argv[flag + 1]
+
+def getTopLimit():
+	try:
+		flag = sys.argv.index('--top')
+		return int(sys.argv[flag + 1])
+	except ValueError:
+		return 10000
+
+inputFile = open('download_stats.json')
+stats = [(k, v) for k, v in json.load(inputFile).items()]
+stats.sort(key=lambda item: item[1], reverse=True)
+stats = stats[:getTopLimit()]
+inputFile.close()
+
+outputFile = open(getOutputFile(), 'w')
+topPackages = [k for k, _ in stats]
+json.dump(topPackages, outputFile, indent=2)
+outputFile.close()
