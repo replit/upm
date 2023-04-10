0dfc27c70c73ac68e59330bc3a05c705abe78a9e
Toby Ho <toby@repl.it>
2023-04-10 17:23:54 -0400

    override for faiss (#103)

--- internal/backends/python/pypi_map.override.go
+++ internal/backends/python/pypi_map.override.go
@@ -8,4 +8,5 @@ var moduleToPypiPackageOverride = map[string]string{
 	"nvd3":         "python-nvd3",         // not popular enough 6th in popularity
 	"requirements": "requirements-parser", // popular rlbot depends on it, but doesn't supply requires_dist
 	"base62":       "pybase62",            // it was overridden by base-62 which wins due to name match but is less popular by far
+	"faiss":        "faiss-cpu",
 }
