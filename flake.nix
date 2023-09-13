bf4e11e09d35c159df9137a0691fc0cd708ec4d9
Colton Donnelly <colton@donn.io>
2023-09-13 21:42:24 +0000

    add node and yarn to devshell, ignore test-suite dir in upm source

--- flake.nix
+++ flake.nix
@@ -11,11 +11,25 @@
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+
+          rev = if self ? rev then "0.0.0-${builtins.substring 0 7 self.rev}" else "0.0.0-dirty";
+
           upm = pkgs.callPackage ./upm.nix {
-            rev = if self ? rev then "0.0.0-${builtins.substring 0 7 self.rev}" else "0.0.0-dirty";
+            inherit rev;
           };
         in
         {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs
+              pkgs.nodePackages.yarn
+            ];
+
+            inputsFrom = [
+              upm
+            ];
+          };
+
           packages = {
             default = upm;
             inherit upm;
