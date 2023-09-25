66dfac9fe878b6dad61be1fe980a5a95ca1b9638
Colton Donnelly <colton@donn.io>
2023-09-24 18:09:38 -0700

    better lock testing

---
+++ test-suite/Lock_test.go
@@ -0,0 +1,44 @@
+package testSuite
+
+import (
+	"testing"
+
+	testUtils "github.com/replit/upm/test-suite/utils"
+)
+
+func TestLock(t *testing.T) {
+	for _, bt := range languageBackends {
+		bt.Start(t)
+		bt.Subtest(bt.Backend.Name, doLock)
+	}
+}
+
+func doLock(bt testUtils.BackendT) {
+	for _, tmpl := range standardTemplates {
+		template := "/" + bt.Backend.Name + "/" + tmpl + "/"
+
+		bt.Subtest(tmpl, func(bt testUtils.BackendT) {
+			bt.AddTestFile(template+bt.Backend.Specfile, bt.Backend.Specfile)
+
+			specDeps := bt.UpmListSpecFile()
+
+			bt.UpmLock()
+
+			lockDeps := bt.UpmListLockFile()
+
+			for _, specDep := range specDeps {
+				found := false
+				for _, lockDep := range lockDeps {
+					if specDep.Name == lockDep.Name {
+						found = true
+						break
+					}
+				}
+
+				if !found {
+					bt.Fail("expected %s in lock file", specDep.Name)
+				}
+			}
+		})
+	}
+}
