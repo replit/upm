f3dccbd2c520d4c907a80a581fa665b3ce314f11
Ryan Mulligan <ryan@ryantm.com>
2023-09-26 11:44:48 -0700

    nix: standardize flake, add devshell, format
    Why
    ===
    * We want to have a nix devshell
    * We don't need to use flake-utils
    * Let's have a Nix formatter
    
    What changed
    ===
    * Remove flake-utils
    * Remove default.nix
    * Adopt a somewhat more standard directory structure for nix files
    * Add a go devshell
    * Use Alejandra for nix fmt
    
    Test plan
    ===
    * upm builds with `nix build -L` and has a reasonable version string

--- flake.nix
+++ flake.nix
@@ -1,25 +1,30 @@
 {
   description = "Universal Package Manager";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    eachSystem = nixpkgs.lib.genAttrs systems;
+    rev =
+      if self ? rev
+      then "0.0.0-${builtins.substring 0 7 self.rev}"
+      else "0.0.0-dirty";
+  in {
+    packages = eachSystem (system:
+      import ./nix {
+        inherit self nixpkgs rev system;
+      });
+    devShells = eachSystem (system: {
+      default = self.packages.${system}.devShell;
+    });
+    formatter = eachSystem (system: self.packages.${system}.fmt);
   };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          upm = pkgs.callPackage ./upm.nix {
-            rev = if self ? rev then "0.0.0-${builtins.substring 0 7 self.rev}" else "0.0.0-dirty";
-          };
-        in
-        {
-          packages = {
-            default = upm;
-            inherit upm;
-          };
-        }
-      );
 }
