9d477be60cfd7a8d59fbf4f8d740a01864ba739c
Colton Donnelly <colton@donn.io>
2023-10-02 15:13:21 -0400

    add package managers to devshell for testing

--- nix/devshell/default.nix
+++ nix/devshell/default.nix
@@ -1,10 +1,17 @@
 {
+  bun,
   go,
   mkShell,
+  nodejs,
+  nodePackages,
 }:
 mkShell {
   name = "upm";
   packages = [
+    bun
     go
+    nodejs
+    nodePackages.pnpm
+    nodePackages.yarn
   ];
 }
