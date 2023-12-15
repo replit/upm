5f062daa7aa60eb49230768045b711a59dfec181
Devon Stewart <devon.stewart@repl.it>
2023-12-15 11:12:12 -0800

    Permit running individual test suites

--- test-suite/Main_test.go
+++ test-suite/Main_test.go
@@ -2,6 +2,8 @@ package testSuite
 
 import (
 	"context"
+	"os"
+	"strings"
 
 	"github.com/replit/upm/internal/backends"
 	"github.com/replit/upm/test-suite/templates"
@@ -20,6 +22,10 @@ func init() {
 	backends.SetupAll()
 
 	for _, bn := range backends.GetBackendNames() {
+		prefix := os.Getenv("UPM_SUITE_PREFIX")
+		if !strings.HasPrefix(bn, prefix) {
+			continue
+		}
 		bt := testUtils.InitBackendT(backends.GetBackend(context.Background(), bn), &templates.FS)
 		languageBackends = append(languageBackends, bt)
 	}
