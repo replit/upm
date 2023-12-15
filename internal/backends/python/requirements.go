b6f1bae7557d7af106cb8e039de5c5a70c17463a
Devon Stewart <devon.stewart@repl.it>
2023-12-15 13:17:19 -0800

    Adding package removal to requirements.go

--- internal/backends/python/requirements.go
+++ internal/backends/python/requirements.go
@@ -92,3 +92,55 @@ func ListRequirementsTxt(path string) ([]PipFlag, map[api.PkgName]api.PkgSpec) {
 	flags, result, _ := recurseRequirementsTxt(0, path, make(map[api.PkgName]api.PkgSpec), make(Constraints))
 	return flags, result
 }
+
+func recurseRemoveFromRequirementsTxt(depth int, path string, pkgs map[api.PkgName]bool) {
+	if depth > 10 {
+		util.Die("Too many -r redirects in %s", path)
+	}
+
+	var lines []string
+
+	handle, err := os.OpenFile(path, os.O_RDWR, 0644)
+	if err != nil {
+		util.Die("%s", err.Error())
+	}
+	defer handle.Close()
+
+	{
+		scanner := bufio.NewScanner(handle)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+
+			if name, _, found := findPackage(line); found && pkgs[normalizePackageName(*name)] {
+				continue
+			} else if nextfile, found := strings.CutPrefix(line, "-r "); found {
+				recurseRemoveFromRequirementsTxt(depth+1, nextfile, pkgs)
+				lines = append(lines, line)
+			} else {
+				lines = append(lines, line)
+			}
+		}
+	}
+
+	err = handle.Truncate(0)
+	if err != nil {
+		util.Die("%s", err.Error())
+	}
+
+	// Truncate does not reset the cursor position
+	_, err = handle.Seek(0, 0)
+	if err != nil {
+		util.Die("%s", err.Error())
+	}
+
+	for _, line := range lines {
+		_, err := handle.WriteString(line + "\n")
+		if err != nil {
+			util.Die("%s", err.Error())
+		}
+	}
+}
+
+func RemoveFromRequirementsTxt(path string, pkgs map[api.PkgName]bool) {
+	recurseRemoveFromRequirementsTxt(0, path, pkgs)
+}
