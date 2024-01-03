6b4c7c2fac258ea52d7ec45060a6463f84125acc
Devon Stewart <devon.stewart@repl.it>
2024-01-03 13:01:25 -0800

    Adding Die variants

--- internal/util/output.go
+++ internal/util/output.go
@@ -28,6 +28,43 @@ func Die(format string, a ...interface{}) {
 	os.Exit(1)
 }
 
+func die(code int, format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format+"\n", a...)
+	os.Exit(code)
+}
+
+func DieIO(format string, a ...interface{}) {
+	die(10, format, a...)
+}
+
+func DieOverwrite(format string, a ...interface{}) {
+	die(11, format, a...)
+}
+
+func DieNetwork(format string, a ...interface{}) {
+	die(12, format, a...)
+}
+
+func DieProtocol(format string, a ...interface{}) {
+	die(13, format, a...)
+}
+
+func DieConsistency(format string, a ...interface{}) {
+	die(14, format, a...)
+}
+
+func DieInitializationError(format string, a ...interface{}) {
+	die(15, format, a...)
+}
+
+func DieUnimplemented(format string, a ...interface{}) {
+	die(15, format, a...)
+}
+
+func DieSubprocess(format string, a ...interface{}) {
+	die(16, format, a...)
+}
+
 // Panicf is a composition of fmt.Sprintf and panic.
 func Panicf(format string, a ...interface{}) {
 	panic(fmt.Sprintf(format, a...))
