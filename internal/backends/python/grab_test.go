22cabced69b743931998a85a65a10a29600349bc
Devon Stewart <devon.stewart@repl.it>
2024-01-04 22:58:26 -0800

    Add Python grab test for elided local modules

--- internal/backends/python/grab_test.go
+++ internal/backends/python/grab_test.go
@@ -3,6 +3,9 @@ package python
 import (
 	"context"
 	"os"
+	"os/exec"
+	"path"
+	"strings"
 	"testing"
 )
 
@@ -53,5 +56,102 @@ import foo #upm package(bar)
 
 	if len(expected) > 0 {
 		t.Errorf("Missing imports %v", expected)
+	}
+}
+
+func TestLocalModules(t *testing.T) {
+	expected := "Hello, world!"
+
+	files := map[string]string{
+		"README.md": "stub",
+		"pyproject.toml": `
+[tool.poetry]
+name = "grab-test"
+version = "0.1.0"
+description = ""
+authors = []
+readme = "README.md"
+packages = [{include="foo", from="src"}]
+
+[tool.poetry.dependencies]
+python = "^3.11"
+
+
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"
+`,
+		"src/foo/__main__.py": `
+import foo.bar.baz
+
+print(foo.bar.baz.blix())
+`,
+		"src/foo/__init__.py":     "",
+		"src/foo/bar/__init__.py": "",
+		"src/foo/bar/baz.py": `
+def blix():
+	return "` + expected + `"
+`,
+		"app.py": ` # TODO(dstewart) Remove this once we re-enable recursion
+import foo.bar.baz
+import flask
+`,
+	}
+
+	testDir := t.TempDir()
+	for fpath, contents := range files {
+		testFile := path.Join(testDir, fpath)
+		err := os.MkdirAll(path.Dir(testFile), 0o755)
+		if err != nil {
+			t.Fatal("unable to create directory", err)
+		}
+		err = os.WriteFile(testFile, []byte(contents), 0o644)
+		if err != nil {
+			t.Fatal("failed to write test file", err)
+		}
+	}
+
+	lastDir, err := os.Getwd()
+	if err != nil {
+		t.Fatal("unable to get cwd", err)
+	}
+	err = os.Chdir(testDir)
+	if err != nil {
+		t.Fatal("unable to get cwd", err)
+	}
+	defer os.Chdir(lastDir)
+
+	// Sanity test, actually run Python in the environment.
+	cmd := exec.Command("bash", "-c", "poetry lock -n; poetry install; poetry run python -m foo")
+	stdout, err := cmd.Output()
+	if err != nil {
+		t.Fatal("failed to execute python", err)
+	}
+	lines := strings.Split(strings.TrimSpace(string(stdout)), "\n")
+	last := lines[len(lines)-1]
+	if last != expected {
+		t.Errorf("Expected %s to equal %s", last, expected)
+	}
+
+	testPypiMap := func(pkg string) (string, bool) {
+		return strings.Split(pkg, ".")[0], true
+	}
+	// Run guess in the directory.
+	pkgs, _ := doGuess(context.Background(), testPypiMap)
+
+	expectedPackages := []string{"flask"}
+	for _, test := range expectedPackages {
+		if _, ok := pkgs[test]; ok {
+			delete(pkgs, test)
+		}
+	}
+
+	unexpectedPkgs := []string{}
+	for extra := range pkgs {
+		unexpectedPkgs = append(unexpectedPkgs, extra)
+	}
+
+	if len(pkgs) > 0 {
+		t.Fatal("guessed extra unexpected packages: ", strings.Join(unexpectedPkgs, ", "))
 	}
 }
