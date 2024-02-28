7e0877841c47054534a1518a89662d61acc16546
Devon Stewart <devon.stewart@repl.it>
2024-02-28 13:06:06 -0800

    Adding tensorflow overrides that point back towards the root

--- internal/backends/python/pypi_map.override.go
+++ internal/backends/python/pypi_map.override.go
@@ -1,18 +1,22 @@
 package python
 
+var tf_variants = [...]string{"tensorflow-cpu", "tensorflow-cpu-aws", "tensorflow-gpu", "tensorflow-intel", "tf-nightly", "tf-nightly-cpu", "tf-nightly-cpu-aws", "tf-nightly-gpu", "tf-nightly-intel"}
+
 /*
 Manual module -> package mapping overrides
 */
 var moduleToPypiPackageOverride = map[string][]string{
-	"grpc_status":  {"grpcio-status"},           // 2nd most popular
-	"nvd3":         {"python-nvd3"},             // not popular enough 6th in popularity
-	"requirements": {"requirements-parser"},     // popular rlbot depends on it, but doesn't supply requires_dist
-	"base62":       {"pybase62"},                // it was overridden by base-62 which wins due to name match but is less popular by far
-	"faiss":        {"faiss-cpu"},               // faiss is offered as precompiled wheels, faiss-cpu and faiss-gpu.
-	"graphics":     {"graphics.py"},             // this package is popular, but the module doesn't match the package name https://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/graphics.html#a-graphics-introduction
-	"replit.ai":    {"replit-ai"},               // Replit's AI package
-	"glm":          {"PyGLM", "glm"},            // Both of these packages are valid, but PyGLM is a library, glm is an executable.
-	"ldclient":     {"ldclient-py", "ldclient"}, // LaunchDarkly client
+	"grpc_status":          {"grpcio-status"},                                 // 2nd most popular
+	"nvd3":                 {"python-nvd3"},                                   // not popular enough 6th in popularity
+	"requirements":         {"requirements-parser"},                           // popular rlbot depends on it, but doesn't supply requires_dist
+	"base62":               {"pybase62"},                                      // it was overridden by base-62 which wins due to name match but is less popular by far
+	"faiss":                {"faiss-cpu"},                                     // faiss is offered as precompiled wheels, faiss-cpu and faiss-gpu.
+	"graphics":             {"graphics.py"},                                   // this package is popular, but the module doesn't match the package name https://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/graphics.html#a-graphics-introduction
+	"replit.ai":            {"replit-ai"},                                     // Replit's AI package
+	"glm":                  {"PyGLM", "glm"},                                  // Both of these packages are valid, but PyGLM is a library, glm is an executable.
+	"ldclient":             {"ldclient-py", "ldclient"},                       // LaunchDarkly client
+	"tensorflow":           append([]string{"tensorflow"}, tf_variants[:]...), // Avoid suggesting conflicting tensorflow builds
+	"tensorflow-federated": {"tensorflow-federated", "tensorflow-federated-nightly"}, // Avoid suggesting conflicting tensorflow-federated packages
 }
 
 /* Proxy packages
