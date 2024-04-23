463e3525ce95e014da12cbd458432af717ed619c
Devon Stewart <devon.stewart@repl.it>
2024-04-23 15:20:49 -0700

    Adding edgartools

--- internal/backends/python/pypi_map.override.go
+++ internal/backends/python/pypi_map.override.go
@@ -10,6 +10,7 @@ var moduleToPypiPackageOverride = map[string][]string{
 	"nvd3":                 {"python-nvd3"},                                          // not popular enough 6th in popularity
 	"requirements":         {"requirements-parser"},                                  // popular rlbot depends on it, but doesn't supply requires_dist
 	"base62":               {"pybase62"},                                             // it was overridden by base-62 which wins due to name match but is less popular by far
+	"edgar": 								{"edgartools", "edgar"},																	// Two different tools to access the Edgar service provided by the SEC, edgartools is more active
 	"faiss":                {"faiss-cpu"},                                            // faiss is offered as precompiled wheels, faiss-cpu and faiss-gpu.
 	"graphics":             {"graphics.py"},                                          // this package is popular, but the module doesn't match the package name https://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/graphics.html#a-graphics-introduction
 	"replit.ai":            {"replit-ai"},                                            // Replit's AI package
