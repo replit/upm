d4d1a76e850a23663803b2dab4af5e3dbf7b1667
Devon Stewart <devon.stewart@repl.it>
2025-04-01 15:45:49 -0700

    Defer evaluation of bunVersion until after bunIsAvailable

--- internal/backends/nodejs/nodejs.go
+++ internal/backends/nodejs/nodejs.go
@@ -683,10 +683,12 @@ var NodejsNPMBackend = api.LanguageBackend{
 
 // BunBackend is a UPM backend for Node.js that uses [Bun](https://bun.sh/).
 func makeBunBackend() api.LanguageBackend {
-	bunVersion := util.SilenceSubroutines(func() any {
-		return util.GetCmdOutput([]string{"bun", "--version"})
-	}).([]byte)
-	newBun := bunIsAvailable() && strings.HasPrefix(string(bunVersion), "1.2")
+	bunVersion := func() []byte {
+		return util.SilenceSubroutines(func() any {
+			return util.GetCmdOutput([]string{"bun", "--version"})
+		}).([]byte)
+	}
+	newBun := bunIsAvailable() && strings.HasPrefix(string(bunVersion()), "1.2")
 	if newBun && commonIsActive("bun.lockb") {
 		fmt.Fprintln(os.Stderr, "Bun's lockfile format has changed from bun.lockb to bun.lock.")
 		util.RunCmd([]string{"bun", "install", "--save-text-lockfile", "--frozen-lockfile", "--lockfile-only"})
